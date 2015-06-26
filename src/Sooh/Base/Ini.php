dd616c7c4c92dcbb9c54f527506b8dccc2adaa47
simon <hillstill@163.com>
2015-06-26 13:29:54 +0800

    add: viewRenderType() add: registerShutdown()

--- src/Sooh/Base/Ini.php
+++ src/Sooh/Base/Ini.php
@@ -20,16 +20,37 @@ class Ini {
 	public function initGobal($arr)
 	{
 		if($this->globals===null){
-			if(is_array($GLOBALS['CONF']))$this->globals=$GLOBALS['CONF'];
-			else $this->globals=array();
+			if(is_array($GLOBALS['CONF'])){
+				$this->globals=$GLOBALS['CONF'];
+			} else {
+				$this->globals=array();
+			}
 		}
-		foreach($arr as $k=>$v)$this->globals[$k]=$v;
+		foreach($arr as $k=>$v){
+			$this->globals[$k]=$v;
+		}
+	}
+	public function viewRenderType($newType=null)
+	{
+		if($newType!==null){
+			$this->initGobal(array('viewRenderType'=>$newType));
+			return $newType;
+		}else{
+			$newType = $this->get('viewRenderType');
+			if(empty($newType)){
+				return 'html';
+			}else{
+				return $newType;
+			}
+		}
 	}
 	public function initCache($basedir,$type=null)
 	{
 		$this->cacheFunc = $type;
 		$this->basedir=$basedir;
-		if($type=='yac')$this->cacheSys = new Yac();
+		if($type=='yac'){
+			$this->cacheSys = new Yac();
+		}
 	}
 	private $cacheFunc=null;
 	private $cacheSys = null;
@@ -39,18 +60,28 @@ class Ini {
 		$v = $this->cacheSys->get($item);
 		if($v===false){
 			$conf = $this->loadFile($item);
-			if(!empty($conf)) $this->cacheSys->set($item,$conf);
+			if(!empty($conf)) {
+				$this->cacheSys->set($item,$conf);
+			}
 			return $conf;
-		}else return $v;
+		}else{
+			return $v;
+		}
+		
 	}
 	protected function yac_del($items)
 	{
 		$realDelete=0;
 		foreach($items as $key){
-			if($this->cacheSys->delete($key))$realDelete++;
+			if($this->cacheSys->delete($key)){
+				$realDelete++;
+			}
 		}
-		if($realDelete===0)return true;
-		else return $realDelete;
+		if($realDelete===0){
+			return true;
+		}else{
+			return $realDelete;
+		}
 	}
 
 	public function delCache($items)
@@ -58,18 +89,26 @@ class Ini {
 		if($this->cacheFunc!==null){
 			$f = $this->cacheFunc.'_del';
 			return $this->$f($items);
-		}else return true;
+		}else{
+			return true;
+		}
 	}
 	protected function loadFile($arg)
 	{
-		if(!is_array($arg))$arg = explode ('.', $arg);
+		if(!is_array($arg)){
+			$arg = explode ('.', $arg);
+		}
 		array_unshift($arg,$this->basedir);
 		return include implode('/', $arg).'.php';
 	}
-	public function get($item)
+	public function get($item, $default=null)
 	{
-		if ($this->globals ===null)$this->globals = $GLOBALS['CONF'];
-		if(isset($this->globals[$item]))return $this->globals[$item];
+		if ($this->globals ===null){
+			$this->globals = $GLOBALS['CONF'];
+		}
+		if(isset($this->globals[$item])){
+			return $this->globals[$item];
+		}
 		$path = explode('.', $item);
 		switch (sizeof($path)){
 			case 3: $conf=isset($this->globals[$path[0]][$path[1]][$path[2]])?$this->globals[$path[0]][$path[1]][$path[2]]:null;break;
@@ -78,15 +117,20 @@ class Ini {
 			default:
 				throw new \ErrorException("change codes support more depth");
 		}
-		if($conf===null && $this->basedir!=null){
+		if($conf===null){
 			if($this->cacheFunc!==null){
 				$f = $this->cacheFunc.'_get';
 				$conf = $this->$f($item);
-			}else{
+			}
+			if($conf === null && $this->basedir!==null){
 				$conf = $this->loadFile($path);
 			}
 		}
+		if($conf===null){
+			return $default;
+		} else {
 		return $conf;
+	}
 	}
 	private static $_instance;
 	/**
@@ -100,5 +144,40 @@ class Ini {
 			self::$_instance = new $class;
 		}
 		return self::$_instance;
+	}
+	/**
+	 * 注册一个shutdown函数（一般是清理函数）
+	 * @param callback $callback array(obj,func), 'class::staticfunc',....
+	 * @param string $funcDesc function desc
+	 */
+	public static function registerShutdown($callback,$funcDesc)
+	{
+		if($callback===null){
+			error_log("try:shutdown:");
+			foreach(self::$funcShutdown as $funcDesc=>$func){
+				try{
+					error_log("try:$funcDesc:");
+					if(is_array($func) || is_string($func)){
+						call_user_func($func);
+					}else{
+						$func();
+					}
+				}  catch (\ErrorException $e){
+					error_log('error shutdown:'.$funcDesc." ".$e->getMessage()."\n".$e->getTraceAsString());
+				}
+			}
+			if(class_exists("\\Sooh\\DB\\Broker",false)){
+				\Sooh\DB\Broker::free();
+			}
+		}else{
+			error_log("add shutdown:$funcDesc:");
+			self::$funcShutdown[$funcDesc]=$callback;
+		}
+	}
+	protected static $funcShutdown=array();
+	
+	public function dump()
+	{
+		return $this->globals;
 	}
 }
