f30b60c83279ecf0389cbaee43551952c63652a3
Simon.Wang <hillstill_simon@163.com>
2015-10-14 15:26:39 +0800

    add toArray() fix support init twice

--- src/Sooh/DB/Pager.php
+++ src/Sooh/DB/Pager.php
@@ -14,28 +14,60 @@ class Pager {
 	private $flgZeroBegin=0;
 	public function __construct($pagesize,$pagesizes=array(),$zeroBegin=false) {
 		$this->flgZeroBegin=$zeroBegin;
-		if(empty($pagesize))$pagesize = current($pagesizes);
-		$this->enumPagesize=implode(',', $pagesizes);
-		if(empty($pagesize))$pagesize = 10;
-		$this->page_size = $pagesize;
+		if(empty($pagesizes)){
+			$this->enumPagesize = $this->page_size=$pagesize-0;
+		}else{
+			if(is_array($pagesizes)){
+				$this->enumPagesize=implode(',', $pagesizes);
+				if(empty($this->page_size)){
+					$this->page_size = current($pagesizes);
+				}
+			}else{
+				$this->enumPagesize = $pagesizes;
+				if(empty($this->page_size)){
+					$pagesizes = explode(',', $pagesizes);
+					$this->page_size = current($pagesizes);
+				}
+			}
+		}
+		if(empty($this->page_size)){
+			$this->page_size = 10;
+		}
 	}
 	/**
 	 * @param int $total
 	 * @param int $pageid
 	 * @return \Sooh\DB\Pager
 	 */
-	public function init($total,$pageid)
+	public function init($total=-1,$pageid=-1)
 	{
-		$this->total = $total-0;
-		if($this->total==0)$this->page_count=1;
-		else $this->page_count = ceil($this->total/$this->page_size);
-		if($this->flgZeroBegin){
-			if($pageid+1>$this->page_count)$this->page_id_zeroBegin=0;
-			else $this->page_id_zeroBegin=$pageid-0;
+		if($pageid===-1){
+			$pageid = $this->flgZeroBegin?$this->page_id_zeroBegin:$this->page_id_zeroBegin+1;
 		}else{
-			if($pageid>$this->page_count || empty($pageid))$this->page_id_zeroBegin=0;
-			else $this->page_id_zeroBegin=$pageid-1;
+			$this->page_id_zeroBegin = ($this->flgZeroBegin)?$pageid:$pageid-1;
 		}
+		if($total!==-1){
+			$this->total = $total-0;
+			if($this->total==0){
+				$this->page_count=1;
+			}else{
+				$this->page_count = ceil($this->total/$this->page_size);
+			}
+			if($this->flgZeroBegin){
+				if($pageid+1>$this->page_count){
+					$this->page_id_zeroBegin=0;
+				}else{
+					$this->page_id_zeroBegin=$pageid-0;
+				}
+			}else{
+				if($pageid>$this->page_count || empty($pageid)){
+					$this->page_id_zeroBegin=0;
+				}else{
+					$this->page_id_zeroBegin=$pageid-1;
+				}
+			}
+		}
+		
 		return $this;
 	}
 	
@@ -51,5 +83,15 @@ class Pager {
 		}else{
 			return $this->page_id_zeroBegin+1;
 		}
+	}
+	
+	public function toArray()
+	{
+		return array(
+			'pageId'=>$this->pageid(),
+			'total'=>$this->total,
+			'pageSize'=>  $this->page_size,
+			'pageCount'=>$this->page_count
+		);
 	}
 }
